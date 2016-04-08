8a670c554558aeddc01083fdb6efa485974d218a
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2016-04-07 20:08:38 -0400

    Catgories model listing added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,7 +21,11 @@ Route::get('/', function () {
 
 Route::get('exemplo','My_Controller@exemplo');
 
-Route::controllers([
-    'auth' => 'Auth\AuthController',
-    'password' => 'Auth\PasswordController',
-]);
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
